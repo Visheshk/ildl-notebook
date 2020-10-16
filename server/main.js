91bb4e76b839148b0625d4a51381e428bd424306
Vishesh Kumar <vishesh@visheshk.net>
2020-10-16 12:09:22 -0400

    committing step 7 of tutorial

---
+++ server/main.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from "/imports/api/TasksCollection";
+
+const insertTask = (taskText, user) => 
+  TasksCollection.insert({ 
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+function insertLink({ title, url }) {
+  LinksCollection.insert({title, url, createdAt: new Date()});
+}
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task'
+    ].forEach(tastText => insertTask(taskText, user));
+  }
+});
